f94ee1e3941757e9be8ba33a0378d34cf0c727d1
yaserzare <info@yaserzare.ir>
2024-05-25 12:49:33 +0330

    Update store method

--- app/Http/Controllers/Admin/User/UserController.php
+++ app/Http/Controllers/Admin/User/UserController.php
@@ -3,20 +3,28 @@
 namespace App\Http\Controllers\Admin\User;
 
 use App\Http\Controllers\Controller;
+use http\Env\Response;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 
 class UserController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-       $users = User::paginate();
-       return response()->json([
-           'data'=>$users
-       ]);
+        $usersQuery = User::query();
+
+        if($request->has('email')) {
+            $usersQuery = $usersQuery->whereEmail($request->email);
+        }
+        $users = $usersQuery->paginate();
+        return response()->json([
+            'data' => $users
+        ]);
 
     }
 
@@ -25,7 +33,30 @@ class UserController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validator = Validator::make($request->all(),[
+            'first_name'=>['required','min:1','max:255'],
+            'last_name'=>['required','min:1','max:255'],
+            'email'=>['required','email','unique:users,email'],
+            'password'=>['required','min:8'],
+        ]);
+
+        if($validator->fails())
+        {
+            return response()->json([
+                'errors'=>$validator->errors()
+            ],422);
+        }
+
+        $inputs = $validator->validated();
+        $inputs['password'] = Hash::make($inputs['password']);
+
+        $user = User::create($inputs);
+
+        return response()->json([
+            'message' => 'User created successfully.',
+            'data' => $user
+        ]);
+
     }
 
     /**
